e4e1b353329c0cfb2142eb641e42162eee6c2947
oyama <ka-ooyama@ous.ac.jp>
2022-12-23 01:30:34 +0900

    add

--- Form1.cs
+++ Form1.cs
@@ -61,30 +61,30 @@ namespace reversi
 
                     if (scene.IsExist(x, y, Reversi.ePLAYER.P0))
                     {
-                        g.FillEllipse(Brushes.White, tmp_x + margine, tmp_y + margine, wh - 2 * margine, wh - 2 * margine);
+                        g.FillEllipse(Brushes.Black, tmp_x + margine, tmp_y + margine, wh - 2 * margine, wh - 2 * margine);
                     }
                     if (scene.IsExist(x, y, Reversi.ePLAYER.P1))
                     {
-                        g.FillEllipse(Brushes.Black, tmp_x + margine, tmp_y + margine, wh - 2 * margine, wh - 2 * margine);
+                        g.FillEllipse(Brushes.White, tmp_x + margine, tmp_y + margine, wh - 2 * margine, wh - 2 * margine);
                     }
                     if (scene.IsPlaceable(x, y, Reversi.ePLAYER.P0))
                     {
-                        g.FillEllipse(Brushes.White, tmp_x + wh / 2 - wh / 8, tmp_y + wh / 2 - wh / 8, wh / 4, wh / 4);
+                        g.FillEllipse(Brushes.Black, tmp_x + wh / 2 - wh / 8, tmp_y + wh / 2 - wh / 8, wh / 4, wh / 4);
                     }
                     if (scene.IsPlaceable(x, y, Reversi.ePLAYER.P1))
                     {
-                        g.FillEllipse(Brushes.Black, tmp_x + wh / 2 - wh / 8, tmp_y + wh / 2 - wh / 8, wh / 4, wh / 4);
+                        g.FillEllipse(Brushes.White, tmp_x + wh / 2 - wh / 8, tmp_y + wh / 2 - wh / 8, wh / 4, wh / 4);
                     }
                 }
             }
 
             if (scene.Player == Reversi.ePLAYER.P0)
             {
-                g.FillEllipse(Brushes.White, (XNum + 1) * wh + wh / 6, 1 * wh + wh / 5, wh / 2, wh / 2);
+                g.FillEllipse(Brushes.Black, (XNum + 1) * wh + wh / 6, 1 * wh + wh / 5, wh / 2, wh / 2);
             }
             else
             {
-                g.FillEllipse(Brushes.Black, (XNum + 1) * wh + wh / 6, 2 * wh + wh / 5, wh / 2, wh / 2);
+                g.FillEllipse(Brushes.White, (XNum + 1) * wh + wh / 6, 2 * wh + wh / 5, wh / 2, wh / 2);
             }
 
             var path = new System.Drawing.Drawing2D.GraphicsPath();
@@ -99,13 +99,13 @@ namespace reversi
             path.AddString(String.Format("{0, 2}", scene.Places(Reversi.ePLAYER.P0)), fnt_seat.FontFamily, (int)fnt_seat.Style, wh / 2, new Point((XNum + 1) * wh, 1 * wh + wh / 2), stringFormat);
 
             //g.DrawPath(new Pen(Color.Black, 8), path);
-            g.FillPath(new SolidBrush(Color.White), path);
+            g.FillPath(new SolidBrush(Color.Black), path);
 
             path.Reset();
             path.AddString(String.Format("{0, 2}", scene.Places(Reversi.ePLAYER.P1)), fnt_seat.FontFamily, (int)fnt_seat.Style, wh / 2, new Point((XNum + 1) * wh, 2 * wh + wh / 2), stringFormat);
 
             //g.DrawPath(new Pen(Color.White, 8), path);
-            g.FillPath(new SolidBrush(Color.Black), path);
+            g.FillPath(new SolidBrush(Color.White), path);
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
